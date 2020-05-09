6f262d74a4d342490a0ae2957287e10752999e8d
akhalifa7 <57017131+akhalifa7@users.noreply.github.com>
2020-05-09 20:34:36 +0200

    Customer Form

--- Code/TransportationDB/DBapplication/CustomerLogin.cs
+++ Code/TransportationDB/DBapplication/CustomerLogin.cs
@@ -11,22 +11,48 @@ namespace DBapplication
 {
     public partial class CustomerLogin : Form
     {
+        Controller controllerObj;
         public CustomerLogin()
         {
             InitializeComponent();
+            controllerObj = new Controller();
         }
 
         private void button1_Login_Click(object sender, EventArgs e)
         {
             //we have to check that the number is in the database
-            int num = 0;   //number got from the textbox
-            Customer C = new Customer(num);
-            C.Show();
+            int phonenumber = int.Parse(textBox1.Text);   //number got from the textbox
+            int searchreturn = controllerObj.SearchForCustomerByNumber(phonenumber);
+
+            if (searchreturn == 1)
+            {
+                MessageBox.Show("This phone number is not registered yet, Please SignUp!");
+            }
+
+            else
+            {
+                Customer C = new Customer(phonenumber);
+                C.Show();
+            }
+
+
         }
 
         private void button2_SignUp_Click(object sender, EventArgs e)
         {
             //adding a new customer
+            //if (int.Parse(textBox2.Text) == NULL)
+
+            int phonenumber = int.Parse(textBox2.Text);
+            string fname = textBox3.Text;
+            string lname = textBox4.Text;
+
+            int SignUpReturn = controllerObj.CustomerSignUp(phonenumber, fname, lname);
+            
+            if (SignUpReturn == 0)
+            {
+                MessageBox.Show("This phone number already registered, Welcome Back!");
+            }
         }
     }
 }
