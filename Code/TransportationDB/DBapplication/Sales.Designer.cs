006233ccde569ba0d1817d0da0c1e66cf9764218
zidano <s-omarzidan@zewailcity.edu.eg>
2020-05-10 06:29:36 +0200

    conflict solves

--- Code/TransportationDB/DBapplication/Sales.Designer.cs
+++ Code/TransportationDB/DBapplication/Sales.Designer.cs
@@ -42,140 +42,153 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-            //
+            // 
             // button1
-            //
+            // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(46, 30);
+            this.button1.Location = new System.Drawing.Point(47, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 66);
+            this.button1.Size = new System.Drawing.Size(100, 54);
             this.button1.TabIndex = 2;
             this.button1.Text = "Get Tracks";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.get_price_click);
-            //
+            // 
             // dataGridView1
-            //
+            // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(320, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(365, 11);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(401, 164);
+            this.dataGridView1.Size = new System.Drawing.Size(301, 133);
             this.dataGridView1.TabIndex = 3;
-            //
+            // 
             // label2
-            //
+            // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(59, 204);
+            this.label2.Location = new System.Drawing.Point(57, 180);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(160, 17);
+            this.label2.Size = new System.Drawing.Size(119, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Enter Customer\'s phone";
-            //
+            // 
             // textBox2
-            //
-            this.textBox2.Location = new System.Drawing.Point(46, 220);
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 193);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
             this.textBox2.TabIndex = 5;
-            //
+            // 
             // dataGridView2
-            //
+            // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(29, 256);
+            this.dataGridView2.Location = new System.Drawing.Point(22, 229);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
-            this.dataGridView2.Size = new System.Drawing.Size(959, 80);
+            this.dataGridView2.Size = new System.Drawing.Size(719, 65);
             this.dataGridView2.TabIndex = 6;
-            //
+            // 
             // button2
-            //
+            // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(46, 90);
+            this.button2.Location = new System.Drawing.Point(47, 90);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 66);
+            this.button2.Size = new System.Drawing.Size(100, 54);
             this.button2.TabIndex = 7;
             this.button2.Text = "Modify Price";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.modify_price_click);
-            //
+            // 
             // textBox3
-            //
-            this.textBox3.Location = new System.Drawing.Point(172, 100);
+            // 
+            this.textBox3.Location = new System.Drawing.Point(199, 110);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 8;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            //
+            // 
             // label3
-            //
+            // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(152, 84);
+            this.label3.Location = new System.Drawing.Point(176, 95);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(191, 17);
+            this.label3.Size = new System.Drawing.Size(144, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "Enter a value to update price";
-            //
+            // 
             // button3
-            //
-            this.button3.Location = new System.Drawing.Point(221, 204);
+            // 
+            this.button3.Location = new System.Drawing.Point(209, 179);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(103, 57);
+            this.button3.Size = new System.Drawing.Size(77, 46);
             this.button3.TabIndex = 10;
             this.button3.Text = "Get Data";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.get_data_click);
-            //
+            // 
             // label4
-            //
+            // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(379, 204);
+            this.label4.Location = new System.Drawing.Point(328, 180);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(154, 17);
+            this.label4.Size = new System.Drawing.Size(116, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Enter new Promo-Code";
-            //
+            // 
             // textBox4
-            //
-            this.textBox4.Location = new System.Drawing.Point(368, 220);
+            // 
+            this.textBox4.Location = new System.Drawing.Point(320, 193);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(185, 22);
+            this.textBox4.Size = new System.Drawing.Size(140, 20);
             this.textBox4.TabIndex = 12;
-            //
+            // 
             // button4
-            //
-            this.button4.Location = new System.Drawing.Point(547, 204);
+            // 
+            this.button4.Location = new System.Drawing.Point(496, 179);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(155, 57);
+            this.button4.Size = new System.Drawing.Size(116, 46);
             this.button4.TabIndex = 13;
             this.button4.Text = "Update Promo-Code";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.update_promo_click);
-            //
+            // 
             // label1
-            //
+            // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(178, 33);
+            this.label1.Location = new System.Drawing.Point(206, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Track ID";
-            //
+            // 
             // textBox1
-            //
-            this.textBox1.Location = new System.Drawing.Point(172, 49);
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 45);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
             this.textBox1.TabIndex = 1;
-            //
+            // 
             // Sales
-            //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 346);
+            this.ClientSize = new System.Drawing.Size(757, 323);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
@@ -190,7 +203,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Sales";
             this.Text = "Sales";
             this.Load += new System.EventHandler(this.Sales_Load);
